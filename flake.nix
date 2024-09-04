3442123d6e8ddee013c6c4e11bd59c73be121ce0
Julian Stieß <j_stiess17@cs.uni-kl.de>
2024-09-04 21:32:37 +0200

    added flake for website

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Setup Stonedstreet";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      src = ./.;
+    in
+      pkgs.stdenv.mkDerivation {
+        pname = "copy-source";
+        version = "0.1";
+
+        src = src;
+
+        installPhase = ''
+          mkdir -p $out
+          cp -r $src $out/
+        '';
+
+        sourceRoot = ".";
+      };
+  };
+}
